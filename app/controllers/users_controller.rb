a23d9e3302f695332840d1dde5b955ddf5ee2845
Piri-kara <hpt_q@yahoo.co.jp>
2019-12-29 00:37:40 +0900

    roomsモデルの変更
    # What
    room_userテーブルを削除
    Roomテーブルにhost_idとclient_idを作成
    Usersテーブルへ紐づけ
    各種viewの編集
    # Why
    後々の変更に強くするため

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @rooms = @user.rooms
     @animals = Animal.where(giver_id: @user.id)
+    #host_idもしくはclient_idが詳細ページもとのuseridであるRoomを抽出
+    @rooms = Room.where("(host_id = ?) OR (client_id = ?)", @user.id, @user.id)
   end
 
   def edit
--- db/migrate/20191227022041_add_column_to_room.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToRoom < ActiveRecord::Migration[5.2]
-  def change
-    add_column :rooms, :status_id, :integer
-  end
-end
